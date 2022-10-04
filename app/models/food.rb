6335ce5fa1d8fb0b1af0441ce14157e917bdfdd5
dqarias <dino.quispea@gmail.com>
2022-10-03 19:02:49 -0500

    Add associations to food, recipe and recipe_food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ApplicationRecord
-  belongs_to :name, class_name: 'User'
+  belongs_to :user, class_name: 'User'
+  has_many :recipe_foods
+  has_many :recipes, through: :recipe_foods
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
-  belongs_to :name, class_name: 'User'
+  belongs_to :user, class_name: 'User'
+  has_many :recipe_foods
+  has_many :foods, through: :recipe_foods
 end
