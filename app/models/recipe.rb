f86362d062ea573ffd86935db2e54db7ee5673de
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-10-04 17:01:47 +0100

    Initial setup

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,3 @@
 class Recipe < ApplicationRecord
-  belongs_to :user, class_name: 'User'
-  has_many :recipe_foods
-  has_many :foods, through: :recipe_foods
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ApplicationRecord
-  has_many :foods, foreign_key: 'user_id', inverse_of: 'user'
-  has_many :recipes, foreign_key: 'user_id', inverse_of: 'user'
 end
