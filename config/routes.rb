c4f55d978faef12ef57084cef4248902303ccdf2
dqarias <dino.quispea@gmail.com>
2022-10-04 17:01:18 -0500

    Change routes according to requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  resources :recipe_foods
-  resources :recipes
-  resources :foods
+  root to: 'users#index'
   resources :users
+  resources :foods, only: %i[index new create destroy]
+  resources :recipes do
+    resources :recipe_foods
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+ 
 end
