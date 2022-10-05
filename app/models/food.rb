d340a80be470fafcb4328d4baf5f1770dd9bb0ce
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-10-05 02:03:48 +0100

    Add validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,8 @@ class Food < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
